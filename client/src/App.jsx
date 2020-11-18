0b2fdd876a7424499f75edfda2e32fd54aa2afaf
Octavio <tudaeva@gmail.com>
2020-11-17 21:23:51 -0800

    implemented left/right buttons to change single card view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,21 +10,30 @@ class App extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      dataPages: []
+      cardObjectsArray: dummydata[0].items,
+      cardObject: dummydata[0].items[0]
     };
+  }
+
+  nextCard () {
+    console.log('clicked Rite!');
+    // debugger;
+    // go to the next card by incrementing the id property on the card
+    // card ids start at 1, so index 0 in the array is id 1
+    // const newIndex = this.state.cardObject.id + 1;
+    this.setState({cardObject: dummydata[0].items[this.state.cardObject.id]});
+  }
+
+  previousCard () {
+    console.log('clicked Left!');
+    // debugger;
+    // const newIndex = this.state.cardObject.id - 2;
+    this.setState({cardObject: dummydata[0].items[this.state.cardObject.id - 2]});
   }
 
   componentDidMount () {
     // e.preventDefault();
-    // axios.get('/product/:id')
-    //   .then((response) => {
-    //     console.log('response from server: ', response.data);
-    //     this.setState({dataPages: response.data[0]});
-    //   })
-
-    this.setState({dataPages: dummydata[0].items});
   }
-
 
   render () {
     return (
@@ -37,14 +46,20 @@ class App extends React.Component {
             </div>
 
             <div>
-              <Card page={this.state.dataPages} />
-              <button type="button">
+              <Card card={this.state.cardObject} />
+              <button type="button" onClick={() => this.previousCard()}
+              // disabled when we're at the first card
+              disabled={this.state.cardObject.id === 1}
+              >Left
                 <svg>
                   <path>
                   </path>
                 </svg>
               </button>
-              <button type="button">
+              <button type="button" onClick={() => this.nextCard()}
+              // disabled when we're at the last card
+              disabled={this.state.cardObject.id === this.state.cardObjectsArray.length}
+              >Right
                 <svg>
                   <path>
                   </path>
@@ -61,10 +76,26 @@ class App extends React.Component {
 
 export default App;
 
-
+// disabled={this.state.firstCardObject.id === 1}
+// disabled={this.state.firstCardObject.id === this.state.cardObjectsArray.length}
 
 // The object I grab from my data depends on the id
 // each path id has to be connected to a specific obj
 // The query in the server side will take the specific id to get the specific object from the db
 // Do I need a router for each type of get request to the db
 // onComponent did mount will take an argument which will be the id, then that id can be passed to the axios request path for the specific router that'll handle that query
+
+  // componentDidMount () {
+    // e.preventDefault();
+    // axios.get('/product/:id')
+    //   .then((response) => {
+    //     console.log('response from server: ', response.data);
+    //     this.setState({cardObjectsArray: response.data[0]});
+    //   })
+
+    // this.setState({cardObjectsArray: dummydata[0].items});
+    // dummydata is an array of objects. each object contains an items property.
+    // items is an array of 12 objects
+    // each object is a an item "card" contains all the ifo for a specific item
+    // set a
+  // }
--- client/src/Card.jsx
+++ client/src/Card.jsx
@@ -1,59 +1,46 @@
 import React from 'react';
 
 const Card = (props) => (
+  <div>
 
-      <ul className="card-set">{
+    <div>
+      <a>
+        <div>
+          <img src={props.card.imageUrl} alt={props.card.imageDescription} />
+        </div>
+      </a>
+    </div>
 
+    <a>
+      <h3>
+        <div>{props.card.brand}</div>
+        <span>{props.card.name}</span>
+      </h3>
+    </a>
 
-      props.page.map((item, index) => (
+    <div>
+      <div>
+        <div>
+          <span>1</span>
+          <span>2</span>
+          <span>3</span>
+          <span>4</span>
+          <span>5</span>
+        </div>
+        <span>
+          <span>{`(${props.card.numberOfRatings})`}</span>
+        </span>
+        <span></span>
+      </div>
+    </div>
 
-        <li className="card" key={index}>
-          <div>
+    <div>
+      <div>
+        <span>{`$${props.card.price}`}</span>
+      </div>
+    </div>
 
-            <div>
-              <a>
-                <div>
-                  <img src={item.imageUrl} alt={item.imageDescription} />
-                </div>
-              </a>
-            </div>
-
-            <a>
-              <h3>
-                <div>{item.brand}</div>
-                <span>{item.name}</span>
-              </h3>
-            </a>
-
-            <div>
-              <div>
-                <div>
-                  <span>1</span>
-                  <span>2</span>
-                  <span>3</span>
-                  <span>4</span>
-                  <span>5</span>
-                </div>
-                <span>
-                  <span>{`(${item.numberOfRatings})`}</span>
-                </span>
-                <span></span>
-              </div>
-            </div>
-
-            <div>
-              <div>
-                <span>{`$${item.price}`}</span>
-              </div>
-            </div>
-
-          </div>
-        </li>
-      ))
-
-      }</ul>
-
-
+  </div>
 );
 
 export default Card;
@@ -90,3 +77,61 @@ export default Card;
 // }
 // <div>This will be the Card</div>
 // </div>
+
+
+// TEMPLATE THAT RECEIVES AN ARRAY OF CARD OBJECTS AND MAPS OVER THEM
+// const Card = (props) => (
+
+//   <ul className="card-set">{
+
+
+//   props.page.map((item, index) => (
+
+//     <li className="card" key={index}>
+//       <div>
+
+//         <div>
+//           <a>
+//             <div>
+//               <img src={item.imageUrl} alt={item.imageDescription} />
+//             </div>
+//           </a>
+//         </div>
+
+//         <a>
+//           <h3>
+//             <div>{item.brand}</div>
+//             <span>{item.name}</span>
+//           </h3>
+//         </a>
+
+//         <div>
+//           <div>
+//             <div>
+//               <span>1</span>
+//               <span>2</span>
+//               <span>3</span>
+//               <span>4</span>
+//               <span>5</span>
+//             </div>
+//             <span>
+//               <span>{`(${item.numberOfRatings})`}</span>
+//             </span>
+//             <span></span>
+//           </div>
+//         </div>
+
+//         <div>
+//           <div>
+//             <span>{`$${item.price}`}</span>
+//           </div>
+//         </div>
+
+//       </div>
+//     </li>
+//   ))
+
+//   }</ul>
+
+
+// );
