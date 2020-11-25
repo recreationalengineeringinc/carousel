190d37423bb7835cd85aa34d3d1fff26d97b1bc1
Octavio <tudaeva@gmail.com>
2020-11-25 12:56:14 -0800

    'EC2 connected. fixed stars classNames to avoid conflicts with groups css'

---
+++ login.sh
@@ -0,0 +1,2 @@
+# ssh -i "hrsf-carousel.pem" ubuntu@ec2-52-43-162-142.us-west-2.compute.amazonaws.com
+ssh -i "/home/octavio/hrsf131-front-end-capstone/hrsf-carousel.pem" ubuntu@ec2-52-43-162-142.us-west-2.compute.amazonaws.com
